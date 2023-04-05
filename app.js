17b958bd66d89510fd3a308c86e2f715d10ade33
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-04-04 23:29:22 -0600

    Se agrego el .gitignore y se creo un server de prueba antes de instalar express

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello world');
+const http = require('http');
+
+http.createServer( (request, response) => {
+
+    response.setHeader('Content-Disposition', 'attachment; filename=list.csv');
+    response.writeHead(200, { 'Content-Type': 'application/csv'});
+
+    response.write( 'id, name');
+    response.write('1, Luis\n');
+    response.write('2, Maria\n');
+    response.write('3, Pedro\n');
+    response.write('4, Luz\n');
+
+    //response.write( JSON.stringify(person) );
+    response.end();
+})
+.listen(8080);
+
+
+console.log('Listening port 8080...');
