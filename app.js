70b5ce4752325e74069cf490356b8f8aca547085
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-04-05 22:40:45 -0600

    Servimos un sitio web en el server, solo contiene 3 paginas y cambiamos el url de acuerdo al nombre de la pagina quitando la extension .html, solo lo hicimos en el index

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/hello', function(req, res) {
     res.send('Hello desde su respectiva ruta');
 });
 
+app.get('/generic', function( req, res) {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', function( req, res ){
+    res.sendFile(__dirname + '/public/elements.html');
+});
+
 app.get('*', function(req, res) {
     res.sendFile(__dirname + '/public/404.html');
 });
