04cf072231135518d8c758d901f5b95b6f2d2817
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-04-05 22:23:17 -0600

    Creamos una pagina de inicio y una pagina 404 para mostrar al usuario que no se pudo encontrar una url que visite no valida, servimos archivos estaticos css para el home

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-http.createServer( (request, response) => {
-
-    response.setHeader('Content-Disposition', 'attachment; filename=list.csv');
-    response.writeHead(200, { 'Content-Type': 'application/csv'});
-
-    response.write( 'id, name');
-    response.write('1, Luis\n');
-    response.write('2, Maria\n');
-    response.write('3, Pedro\n');
-    response.write('4, Luz\n');
-
-    //response.write( JSON.stringify(person) );
-    response.end();
-})
-.listen(8080);
+// Servir mi contenido estatico
+app.use( express.static('public') );
 
 
-console.log('Listening port 8080...');
+app.get('/hello', function(req, res) {
+    res.send('Hello desde su respectiva ruta');
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/public/404.html');
+});
+app.listen(8080)
