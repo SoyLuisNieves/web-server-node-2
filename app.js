17188c39ef82d08ee50cf382f64a33d9d2cce1a8
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-04-06 16:19:12 -0600

    Utilizamos partials configurando el directorio el cual utilizaremos para guardar nuestros partials, cambiamos las peticiones para las paginas generic y elements, ya no enviamos en arcvhio a mostrar, ahora los renderizamos y mandamos algunos argumentos con informacion de la pagina

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 const express = require('express');
-const app = express();
+const hbs = require('hbs');
 
+const app = express();
 // TODO: require('hbs');
+
+// Handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
 // Servir mi contenido estatico
 app.use( express.static('public') );
 
-
 app.get('/', function(req, res) {
-    res.render('home');
+    res.render('home', {
+        name: 'Luis Nieves',
+        title: 'Web Server'
+    });
 });
+
+app.get('/elements', function(req,res) {
+    res.render('elements', {
+        name: 'Luis Nieves Elements',
+        title: 'Elements',
+    })
+});
+
+app.get('/generic', function(req, res) {
+    res.render('generic', {
+        name: 'Luis Nieves Generic',
+        title: 'Generic'
+    })
+})
 
 app.get('/generic', function( req, res) {
     res.sendFile(__dirname + '/public/generic.html');
 });
 
-app.get('/elements', function( req, res ){
-    res.sendFile(__dirname + '/public/elements.html');
-});
 
 app.get('*', function(req, res) {
     res.sendFile(__dirname + '/public/404.html');
 });
-app.listen(8080)
+app.listen(8080);
